2c52b3cd14f34531deff68e215910052b7520c5e
Joshua M <72484879+warybyte@users.noreply.github.com>
2021-05-27 12:17:54 -0400

    Create yumval.sh

---
+++ yumval.sh
@@ -0,0 +1,18 @@
+# Date: May 27, 2021
+# Purpose: Verify if RHEL system has been updated in >45 days
+# Dependencies: Remote execution over ssh is advised
+#
+#!/bin/bash
+# Determine date of last 'Erase, Install, Update' from yum history
+yumpull=$(sudo yum history 2>&1 | grep "E, I, U" | head -1 | awk -F ' ' '{print $6}');
+
+# Determine 45 day mark from last Yum update
+thirtyday=$(date -d "$yumpull+30day" +%s);
+
+# Compare Epoch conversion of one date from the other
+if [ $(date +%s) -ge $thirtyday ];
+then
+        echo "Warning: Last update outside 45 days.";
+else
+        echo "Clear: Last update within 45 days.";
+fi
