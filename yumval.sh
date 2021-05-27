0005828a719a0d2f81be9c65297b8e546caf78af
Joshua M <72484879+warybyte@users.noreply.github.com>
2021-05-27 12:24:20 -0400

    Update yumval.sh

--- yumval.sh
+++ yumval.sh
@@ -7,10 +7,10 @@
 yumpull=$(sudo yum history 2>&1 | grep "E, I, U" | head -1 | awk -F ' ' '{print $6}');
 
 # Determine 45 day mark from last Yum update
-thirtyday=$(date -d "$yumpull+30day" +%s);
+updaterange=$(date -d "$yumpull+45day" +%s);
 
 # Compare Epoch conversion of one date from the other
-if [ $(date +%s) -ge $thirtyday ];
+if [ $(date +%s) -ge $updaterange ];
 then
         echo "Warning: Last update outside 45 days.";
 else
